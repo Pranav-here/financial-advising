d8274f443dca880565aa47028a77ebea80502bc0
Pranav-here <113384349+Pranav-here@users.noreply.github.com>
2025-06-07 16:11:38 +0530

    The complete Financial Agent

--- testing.py
+++ testing.py
@@ -4,11 +4,22 @@ load_dotenv()  # Force load environment variables from .env
 
 from phi.agent import Agent
 from phi.model.groq import Groq
+from phi.tools.yfinance import YFinanceTools
+
 
 test_agent = Agent(
     model=Groq(
         id="llama-3.3-70b-versatile"
-    )
+    ),
+    tools=[YFinanceTools(
+        stock_price=True,
+        analyst_recommendations = True,
+        stock_fundamentals=True
+    )],
+    show_tool_calls=True,
+    markdown=True,
+    instructions=["Always create tables for comparisions"],
+    debug_mode=True
 )
 
-test_agent.print_response("What is your knowledge cutoff date?")
+test_agent.print_response("Summarize and compare the analyst recommendations for the stocks of Apple and Tesla?")
