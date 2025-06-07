9494ce8ce55aa0bd87cb473e9ee8be0a9f349b71
Pranav-here <113384349+Pranav-here@users.noreply.github.com>
2025-06-07 15:32:22 +0530

    v1.1
    fixed bug --> env variables not loading

--- testing.py
+++ testing.py
@@ -1,9 +1,13 @@
+from dotenv import load_dotenv
+import os
+load_dotenv()  # Force load environment variables from .env
+
 from phi.agent import Agent
 from phi.model.groq import Groq
 
 test_agent = Agent(
     model=Groq(
-        id='llama-3.3-70b-versatile'
+        id="llama-3.3-70b-versatile"
     )
 )
 
